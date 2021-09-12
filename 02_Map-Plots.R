72251247df4036fb3d7b73183d148cf66bc93bd4
jakepscott <jakepscott16@gmail.com>
2021-09-12 18:55:05 -0400

    Made map yearly

--- 02_Map-Plots.R
+++ 02_Map-Plots.R
@@ -30,7 +30,8 @@ data <- data_raw %>%
                names_to = "date", 
                #label the values column whatever the variable is
                values_to = data_raw$`Series Name`[1]) %>% 
-  clean_names()
+  clean_names() %>% 
+  select(imf_country_code, date, headline_consumer_price_index)
 
 #Set up the date column
 data <- data  %>% 
@@ -45,37 +46,35 @@ data <- data %>%
 
 #Make a variable for each 5 year period
 data <- data %>%
-  mutate(decade = case_when(year(date) < 1985 ~ "1980-1984",
-                            year(date) >= 1985 & year(date) <1990 ~ "1985-1989",
-                            year(date) >=1990 & year(date) <1995 ~ "1990-1994's",
-                            year(date) >=1995 & year(date) <2000 ~ "1995-1999",
-                            year(date) >=2000 & year(date) <2005 ~ "2000-2004",
-                            year(date) >=2005 & year(date) <2010 ~ "2005-2009",
-                            year(date) >=2010 & year(date) <2015 ~ "2010-2014",
-                            year(date) >=2015 ~ "2015-2020")) %>% 
   group_by(iso_code) %>% 
   #Calculate the actual inflation rate
   mutate(inflation = (headline_consumer_price_index - lag(headline_consumer_price_index,12))/lag(headline_consumer_price_index,12)*100) %>% 
   ungroup()
 
+#Get yearly inflation by taking the average of the 12 quarter inflation by quarter
+data_agg <- data %>% 
+  group_by(iso_code, year) %>% 
+  summarise(inflation = mean(inflation, na.rm = T)) %>% 
+  ungroup()
+
 # Create inflation categories
-data <- data %>% 
+data_agg <- data_agg %>% 
   mutate(inflation_level = case_when(inflation > 50 ~ ">50",
                                      #inflation >= 50 & inflation < 100 ~ "Super High",
                                      #inflation >= 25 & inflation < 50 ~ "Very High",
-                                     inflation >= 10 & inflation < 50 ~ "10-50",
-                                     inflation >= 2.5 & inflation < 10 ~ "5-10",
-                                     inflation >= 1 & inflation < 2.5 ~ "1-5",
-                                     inflation < 1 ~ "<1",
+                                     inflation >= 10 & inflation < 50 ~ "10-49",
+                                     inflation >= 5 & inflation < 10 ~ "5-9",
+                                     inflation >= 0.5 & inflation < 5 ~ "0.5-4",
+                                     inflation < 0.5 ~ "<0.5",
                                      T ~ NA_character_))
 
 # Make the inflation categories into an order factor
-data$inflation_level <- factor(data_agg$inflation_level, ordered = T,
-                               labels = c(">50","10-50","5-10",'1-5',"<1"),
-                               levels = c(">50","10-50","5-10",'1-5',"<1"))
+data_agg$inflation_level <- factor(data_agg$inflation_level, ordered = T,
+                               labels = c(">50","10-49","5-9",'0.5-4',"<0.5"),
+                               levels = c(">50","10-49","5-9",'0.5-4',"<0.5"))
 
 #Join in the world data
-joined <- test_agg %>%
+joined <- data_agg %>%
   right_join(world, by = c("iso_code"="iso_a3")) %>%
   st_as_sf()
 
@@ -83,14 +82,25 @@ joined <- test_agg %>%
 # Plotting ----------------------------------------------------------------
 
 map_anim <- joined %>% 
-  #filter(year == 2018) %>% 
+  filter(year > 1970 & year < 2021) %>% 
   ggplot(aes(fill = inflation_level)) +
   geom_sf(fill = "grey50") +
   geom_sf(aes(fill = inflation_level)) +
+  guides(fill = guide_legend(nrow = 1)) + 
   scale_fill_brewer(palette =  "YlOrRd", direction = -1, na.value = "grey") +
   transition_manual(year) +
-  labs(title = "Headline CPI in`: {current_frame}")
+  labs(title = "Headline CPI in: {current_frame}") +
+  theme_void() +
+  theme(legend.position = "top")
+
+
+
+#animate(plot = map_anim, 
+#          end_pause = 1)
 
 anim_save(animation = map_anim, 
           filename = "Yearly_inflation_map.gif", 
-          path = here("figures/"),end_pause = 30)
+          path = here("figures/"),
+          end_pause = 12,
+          nframes = 100, 
+          fps = 5)
