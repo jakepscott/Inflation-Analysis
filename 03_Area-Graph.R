e0af5de03ac90feca1acfd7a349140f90acec55f
jakepscott <jakepscott16@gmail.com>
2021-09-13 17:55:18 -0400

    Make faceted line plot look pretty

--- 03_Area-Graph.R
+++ 03_Area-Graph.R
@@ -65,8 +65,16 @@ plot_data %>%
 
 
 # Line Plot ---------------------------------------------------------------
-my_pal = brewer.pal(n = 9, "YlOrRd")[5:9] 
+# See how many people were in each inflation category by decade, for subtitle
+plot_data %>% mutate(decade = case_when(year < 1980 ~ "1970's",
+                                        year >= 1980 & year <1990 ~ "1980's",
+                                        year >= 1990 & year <2000 ~ "1990's",
+                                        year >=2000 & year <2010 ~ "2000's",
+                                        year >=2010 ~ "2010's")) %>% 
+  group_by(decade, inflation_level) %>% 
+  summarise(mean = mean(percent_pop)) 
 
+my_pal = brewer.pal(n = 9, "YlOrRd")[5:9] 
 
 plot_data %>% 
   #filter(inflation_level == "1-3") %>% 
@@ -76,18 +84,18 @@ plot_data %>%
   scale_color_manual(values = my_pal) +
   scale_y_continuous(labels = scales::percent) +
   facet_wrap(~inflation_level) +
-  labs(title = "The percent of people living in countries with extremely high inflation has steadily declined since the 1970s",
+  labs(title = "The percent of people living in countries with low to moderate inflation rates has increased notably since the 1970s",
+       subtitle = "The percent of people living in countries where annual inflation is below 3% has risen from just 12% to 42%, while the number living in countries <br>where annual inflation is 10% or high has declined from 40% to 13%",
        y = "Percent of Population",
        x = NULL,
        caption = "Plot: @jakepscott2020 | Data: World Bank via Ha, Kose, & Ohnsorge (2021)") +
   theme_minimal(base_size = 12, base_family = "Roboto Condensed") +
   theme(plot.title = element_markdown(face = "bold", size = rel(1.2)),
-        plot.subtitle = element_text(face = "plain", size = rel(1), color = "grey70"),
+        plot.subtitle = element_markdown(face = "plain", size = rel(1), color = "grey30"),
         plot.caption = element_text(face = "italic", size = rel(0.8), 
                                     color = "grey50"),
         legend.position = "none",
         axis.text.x = element_text(size=rel(1)),
-        axis.text.y = element_blank(),
         plot.title.position = "plot")
 
 ggsave(here("figures/percent_of_pop_lines.png"), dpi = 600, bg = "white", 
